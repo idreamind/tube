8e4513717632b0b7e2bb5f27954d32f6a57d2a40
dreamind <kos.voit@gmail.com>
2014-12-30 22:53:45 +0300

    Tube init

---
+++ js/particles/particles.js
@@ -0,0 +1,18 @@
+/**
+ * Created by dreamind on 23.12.2014.
+ */
+
+//(function(){
+//    $(document).ready(function() {
+//        var color = '#FFD28A'; // #FFAD29
+//
+//        $('.play-particle').particleground({
+//            dotColor: color,
+//            lineColor: color,
+//            maxSpeedX: 0.1,
+//            maxSpeedY: 0.1,
+//            density: 4500,
+//            parallax: false
+//        });
+//    });
+//})()
